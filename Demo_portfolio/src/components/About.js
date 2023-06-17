0fb352e31fcde13d1120b46699ed26530b4e31f8
sweiler6420 <39660360+sweiler6420@users.noreply.github.com>
2023-06-17 00:48:11 -0400

    fixed typo :)

--- Demo_portfolio/src/components/About.js
+++ Demo_portfolio/src/components/About.js
@@ -5,7 +5,7 @@ export default function About() {
         <div>
             <h3 className="about-title" >About</h3>
             <p className="about-text">
-                I am a full stack developer a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.
+                I am a full stack developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.
             </p>
             <h3 className="interest-title" >Interests</h3>
             <p className="interest-text">
